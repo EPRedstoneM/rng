6a2c171f94318db3488b2f1f91c5237c26dae31f
EPRedstoneM <76868698+EPRedstoneM@users.noreply.github.com>
2021-01-02 15:31:53 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const readline = require('readline').createInterface({
 readline.question('What do you want as the highest possible number?', ans => {
   if (isNaN(ans)) {
     console.log('Please specify a valid number.')
+    process.exit()
     return
   }
 
 const answ = ans - 1
   
     console.log(`Your random number is: ${1 + Math.floor(Math.random() * answ)}`)
+    process.exit()
 })
+
 
